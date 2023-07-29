45ec43e7a09f78440e6a59f191267ca630fc20d2
Nicolas Stevens <stvns.ncls@gmail.com>
2023-07-29 13:52:17 +0200

    Clear first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("school3 JS imported successfully!");
+  console.log("Javascript is loaded");
 });
