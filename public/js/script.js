766ff0c556e8c8240d74a2f97e3713b7644928e8
Nicolas Stevens <stvns.ncls@gmail.com>
2023-07-30 21:19:37 +0200

    Clear code CSS, JS, HTML

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // const errorMessage = document.querySelector(
   //   ".form__container #error-message"
   // );
-
   // nextButton.addEventListener("click", (e) => {
   //   console.log("lol");
   //   if (firstName.value && lastName.value) {
@@ -29,23 +28,4 @@ document.addEventListener("DOMContentLoaded", () => {
   //     `;
   //   }
   // });
-
-  /* NAVBAR MENU */
-  const navbarMenuBtn = document.querySelector("#navbar-menu-btn");
-  const navbarMenu = document.querySelector("#navbar-menu");
-  const navbarMenuClose = document.querySelector("#navbar-menu-close");
-
-  navbarMenuBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("navbar__user__menu-active");
-  });
-
-  navbarMenuClose.addEventListener("click", () => {
-    navbarMenu.classList.toggle("navbar__user__menu-active");
-  });
-
-  window.addEventListener("click", function (e) {
-    if (!navbarMenu.contains(e.target) && !navbarMenuBtn.contains(e.target)) {
-      navbarMenu.classList.remove("navbar__user__menu-active");
-    }
-  });
 });
