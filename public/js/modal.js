43a60956418ad39d80b74809ec06dd2ec7ad5108
Nicolas Stevens <stvns.ncls@gmail.com>
2023-08-08 20:24:11 +0200

    responsive for lg screen

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleModalButton = document.querySelector("#open-modal-button");
-  const toggleModalIcon = document.querySelector("#open-modal-button span");
   const closeModalButton = document.querySelector(".modal__close");
   const modal = document.querySelector(".modal__container");
+  const modalBackground = document.querySelector(".modal");
 
-  window.addEventListener("click", function (e) {
-    if (e.target === toggleModalButton || e.target === toggleModalIcon) {
-      modal.classList.remove("modal__container-hidden");
-      modal.parentNode.classList.remove("modal__hidden");
-    } else if (e.target === closeModalButton) {
-      modal.classList.add("modal__container-hidden");
-      modal.parentNode.classList.add("modal__hidden");
-    } else if (
-      !closeModalButton.contains(e.target) &&
-      !modal.contains(e.target)
-    ) {
+  toggleModalButton.addEventListener("click", () => {
+    modal.classList.remove("modal__container-hidden");
+    modal.parentNode.classList.remove("modal__hidden");
+  });
+
+  closeModalButton.addEventListener("click", () => {
+    modal.classList.add("modal__container-hidden");
+    modal.parentNode.classList.add("modal__hidden");
+  });
+
+  modalBackground.addEventListener("click", function (e) {
+    if (!closeModalButton.contains(e.target) && !modal.contains(e.target)) {
       modal.classList.add("modal__container-hidden");
       modal.parentNode.classList.add("modal__hidden");
     }
